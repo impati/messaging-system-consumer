be6ec96ae505039d15382c30c8104f53318c4950
impati <yongs170@naver.com>
2025-04-20 22:25:33 +0900

    first commit

---
+++ src/main/java/com/example/impati/messaging_system_consumer/MessagingSystemConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.impati.messaging_system_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingSystemConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingSystemConsumerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/impati/messaging_system_consumer/MessagingSystemConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.impati.messaging_system_consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingSystemConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
