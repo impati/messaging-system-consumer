5b579dbf7cbd83e1cb154a3ef457269202b740af
impati <yongs170@naver.com>
2025-05-01 00:06:59 +0900

    feature: version up 1.1.3

--- src/main/java/com/example/impati/messaging_system_consumer/core/SimpleMessagingSystemConsumer.java
+++ src/main/java/com/example/impati/messaging_system_consumer/core/SimpleMessagingSystemConsumer.java
@@ -1,30 +1,36 @@
 package com.example.impati.messaging_system_consumer.core;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import java.util.List;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 public class SimpleMessagingSystemConsumer<T> implements MessagingSystemConsumer<T> {
 
     private final WebClient webClient;
+    private final ObjectMapper mapper;
 
     public SimpleMessagingSystemConsumer(WebClient.Builder webClientBuilder,
-                                         MessagingSystemProperties properties) {
+                                         MessagingSystemProperties properties,
+                                         ObjectMapper mapper) {
+        this.mapper = mapper;
         this.webClient = webClientBuilder.baseUrl(properties.url()).build();
     }
 
+    @SuppressWarnings("unchecked")
     public Mono<List<T>> consume(Channel channel) {
         String consumerId = Client.getInstance().getConsumerId(channel);
         return webClient.get()
                 .uri("/v1/consume/{id}", consumerId)
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<MessageResponses<T>>() {
-                })
-                .flatMapIterable(MessageResponses::messages)
-                .map(MessageResponse::data)
-                .collectList();
+                .bodyToMono(MessageResponses.class)
+                .map(responses ->
+                        responses.messages().stream()
+                                .map(data -> mapper.convertValue(data, new TypeReference<T>() {
+                                }))
+                                .toList());
     }
 
     record MessageResponses<T>(
