75064fa3c36bf200a09ff6fd595ce572c87838d0
impati <yongs170@naver.com>
2025-05-01 12:02:58 +0900

    feature: version up 1.1.4

--- src/main/java/com/example/impati/messaging_system_consumer/core/SimpleMessagingSystemConsumer.java
+++ src/main/java/com/example/impati/messaging_system_consumer/core/SimpleMessagingSystemConsumer.java
@@ -1,22 +1,29 @@
 package com.example.impati.messaging_system_consumer.core;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import java.time.LocalDateTime;
 import java.util.List;
+import org.springframework.core.ResolvableType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 public class SimpleMessagingSystemConsumer<T> implements MessagingSystemConsumer<T> {
 
     private final WebClient webClient;
-    private final ObjectMapper mapper;
+    private final ObjectMapper objectMapper;
+    private final JavaType dataType;
 
     public SimpleMessagingSystemConsumer(WebClient.Builder webClientBuilder,
                                          MessagingSystemProperties properties,
-                                         ObjectMapper mapper) {
-        this.mapper = mapper;
+                                         ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
         this.webClient = webClientBuilder.baseUrl(properties.url()).build();
+        this.dataType = TypeFactory.defaultInstance().constructType(
+                ResolvableType.forClass(SimpleMessagingSystemConsumer.class, getClass())
+                        .getGeneric(0)
+                        .getType());
     }
 
     @SuppressWarnings("unchecked")
@@ -28,8 +35,7 @@ public class SimpleMessagingSystemConsumer<T> implements MessagingSystemConsumer
                 .bodyToMono(MessageResponses.class)
                 .map(responses ->
                         responses.messages().stream()
-                                .map(data -> mapper.convertValue(data, new TypeReference<T>() {
-                                }))
+                                .map(msg -> objectMapper.convertValue(msg, dataType))
                                 .toList());
     }
 
